7406119b93ce5316c905608bcba70b2e272c9c79
Sma-source <rabhi.smain@gmail.com>
2021-01-06 18:56:46 +0100

    rendering button and mapping levels to it

--- src/Levels.js
+++ src/Levels.js
@@ -1,5 +1,22 @@
 import React from "react";
 const Levels = ({ levels, filterQuizz }) => {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        {levels.map((level, index) => {
+          return (
+            <Button
+              size="md"
+              className="m-1"
+              key={index}
+              onClick={() => filterQuizz(level)}
+            >
+              {level}
+            </Button>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 export default Levels;
