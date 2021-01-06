697d974270e2b2963d0b2c17131f38693a9050d8
Sma-source <rabhi.smain@gmail.com>
2021-01-06 16:28:48 +0100

    update data to add levels

--- src/data.js
+++ src/data.js
@@ -3,6 +3,7 @@ const data = [
     questionText: "How many Rings of Power have been forged ?",
     img:
       "http://vignette1.wikia.nocookie.net/lego-lord-of-the-rings/images/4/4c/The_One_Ring.jpg/revision/latest?cb=20130113172456",
+    levels: "Medium",
     answerOptions: [
       { answerText: "1", isCorrect: false },
       { answerText: "3", isCorrect: false },
@@ -15,6 +16,7 @@ const data = [
       "Who is committing suicide in the besieged city of Minas Tirith during the Battle of the Pelennor Fields ?",
     img:
       "https://www.telegraph.co.uk/content/dam/film/lord-of-the-rings/minas-tirith-medium.jpg",
+    levels: "Medium",
 
     answerOptions: [
       { answerText: "Ecthelion II", isCorrect: false },
@@ -26,6 +28,7 @@ const data = [
   {
     questionText: "What is the name of the fortress of the Dark Lord Sauron ?",
     img: "https://live.staticflickr.com/3828/9845910735_29cb554c11_b.jpg",
+    levels: "Easy",
     answerOptions: [
       { answerText: "Cirith Ungol", isCorrect: false },
       { answerText: "Minas Morgul", isCorrect: false },
@@ -36,6 +39,7 @@ const data = [
   {
     questionText: "Who was the king of the rohan?",
     img: "https://i.ytimg.com/vi/qByDVgc8R6E/maxresdefault.jpg",
+    levels: "Hard",
     answerOptions: [
       { answerText: "Théoden", isCorrect: true },
       { answerText: "Théodred", isCorrect: false },
@@ -48,6 +52,7 @@ const data = [
     questionText:
       "What was Gollum name befored he was corrupted by the One ring",
     img: "https://lauralzimmerman.files.wordpress.com/2015/04/gollum.gif",
+    levels: "Easy",
     answerOptions: [
       { answerText: "Smog", isCorrect: false },
       { answerText: "Déagol", isCorrect: false },
@@ -60,6 +65,7 @@ const data = [
     questionText: "How many are the Black Riders also known as The Nazgûl",
     img:
       "http://i1.wp.com/31.media.tumblr.com/0b3b0f3d7bc886f51a2a08dbb660c2bf/tumblr_ml67l0szs71rnvb0co1_500.gif?resize=428%2C231",
+    levels: "Hard",
     answerOptions: [
       { answerText: "7", isCorrect: false },
       { answerText: "20", isCorrect: false },
