0146e213c266268b9f35b32fa6ee955777ac5dff
Sma-source <rabhi.smain@gmail.com>
2021-01-07 16:14:46 +0100

    add style

--- src/Levels.js
+++ src/Levels.js
@@ -2,13 +2,39 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 const Levels = ({ levels, filterQuizz }) => {
   return (
-    <>
-      <div className="container text-center">
+    <section className="full">
+      <div className="box container text-center">
+        <span className="text-dark font-weight-bold">Quizz</span>
+        <h1 className="mt-4">
+          <span className="text-light display-4 font-weight-bold"> The</span>{" "}
+          <br />
+          <span className="text-dark display-1 font-weight-bold">
+            {" "}
+            Lord
+          </span>{" "}
+          <br />
+          <span className="text-light display-4 font-weight-bold">
+            of The
+          </span>{" "}
+          <br />
+          <span className="text-dark display-1 font-weight-bold">
+            {" "}
+            Rings
+          </span>{" "}
+          <br />
+        </h1>
+        <img
+          src="https://icon-library.com/images/lord-of-the-rings-icon/lord-of-the-rings-icon-19.jpg"
+          width="150"
+          alt="ring"
+          className="img-fluid mt-2"
+        />
+        <h3 className="mt-5 mb-5">Choose a level</h3>
         {levels.map((level, index) => {
           return (
             <Button
-              size="md"
-              className="m-1"
+              size="lg"
+              className="btn-level m-1"
               key={index}
               onClick={() => filterQuizz(level)}
             >
@@ -17,7 +43,7 @@ const Levels = ({ levels, filterQuizz }) => {
           );
         })}
       </div>
-    </>
+    </section>
   );
 };
 export default Levels;
