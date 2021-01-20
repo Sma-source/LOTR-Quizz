8d56529d6c8f0634705573f9af8382eebf324dc1
Sma-source <rabhi.smain@gmail.com>
2021-01-20 19:09:30 +0100

    update globalstate.js: delete unuse

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer, useState } from "react";
+import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
 
 // Init State
