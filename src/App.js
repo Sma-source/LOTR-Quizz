b2d59df37d52371116cfcaf0115a71c6f337e891
Sma-source <rabhi.smain@gmail.com>
2021-02-04 22:36:12 +0100

    import Gameover component and render it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./index.css";
-
 import Question from "./Question";
 import Answers from "./Answers";
+import Gameover from "./Gameover";
 import Alert from "./Alert";
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import Jumbotron from "react-bootstrap/Jumbotron";
 import Row from "react-bootstrap/Row";
 import Levels from "./Levels";
 import { useGlobalContext } from "./context/GlobalState";
@@ -74,26 +72,7 @@ function App() {
       {start ? (
         <Levels />
       ) : showScore ? (
-        <Container className="p-3 d-flex align-items-center min-vh-100">
-          <Jumbotron className="score text-center mx-auto">
-            <h1 className="header mb-5 display-1">
-              Your Score <br></br>
-              {score}/{quizz.length}
-            </h1>
-            {/* {alert.show && <Alert {...alert} />} */}
-
-            <Button variant="secondary" size="lg" onClick={handleStart}>
-              Try another Level
-            </Button>
-            <Button
-              className="btn-brown"
-              size="lg m-2"
-              onClick={handleTryAgain}
-            >
-              Try again
-            </Button>
-          </Jumbotron>
-        </Container>
+        <Gameover />
       ) : (
         <>
           <Container>
