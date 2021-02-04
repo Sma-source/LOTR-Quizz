a063de5b2a42dd1d01863ede8f4de494bfbb25fd
Sma-source <rabhi.smain@gmail.com>
2021-02-04 22:39:12 +0100

    delete unuse code

--- src/App.js
+++ src/App.js
@@ -3,68 +3,15 @@ import "./index.css";
 import Question from "./Question";
 import Answers from "./Answers";
 import Gameover from "./Gameover";
-import Alert from "./Alert";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Levels from "./Levels";
 import { useGlobalContext } from "./context/GlobalState";
 function App() {
-  const {
-    score,
-    showScore,
-    quizz,
-    start,
-    handleTryAgain,
-    handleStart,
-  } = useGlobalContext();
-  // const { Questionnaire } = useContext(GlobalContext);
-  // const [quizz, setQuizz] = useState(Questionnaire);
-
-  // const [start, setStart] = useState(true);
-
-  // const [currentQuestion, setCurrentQuestion] = useState(0);
-
-  // const [showScore, setShowScore] = useState(false);
-
-  // const [score, setScore] = useState(0);
-
-  // const [alert, setAlert] = useState({
-  //   show: false,
-  //   msg: "",
-  //   type: "",
-  // });
-  // const filterQuizz = (difficulties) => {
-  //   const newQuizz = Questionnaire.filter(
-  //     (datas) => datas.difficulties === difficulties
-  //   );
-  //   setQuizz(newQuizz);
-  //   setStart(false);
-  // };
-  // const handleAnswerClick = (isCorrect) => {
-  //   if (isCorrect) {
-  //     setScore(score + 1);
-  //   }
-  //   const nextQuestion = currentQuestion + 1;
-  //   if (nextQuestion < quizz.length) {
-  //     setCurrentQuestion(nextQuestion);
-  //   } else {
-  //     setShowScore(true);
-  //   }
-  // };
-
-  // const handleTryAgain = () => {
-  //   setShowScore(false);
-  //   setCurrentQuestion(0);
-  //   setScore(0);
-  // };
+  const { showScore, start } = useGlobalContext();
 
   // const showAlert = (show = false, type = "", msg = "") => {
   //   setAlert({ show, type, msg });
-  // };
-
-  // const handleStart = () => {
-  //   setStart(true);
-  //   handleTryAgain();
   // };
 
   return (
