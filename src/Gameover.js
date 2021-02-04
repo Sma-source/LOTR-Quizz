2027bb4c2d9a50e6c02405ccfa3ec35af73f3c44
Sma-source <rabhi.smain@gmail.com>
2021-02-04 22:31:26 +0100

    modify Gameover.js

--- src/Gameover.js
+++ src/Gameover.js
@@ -2,7 +2,26 @@ import React from "react";
 import { useGlobalContext } from "./context/GlobalState";
 const Gameover = () => {
   const { quizz, score, handleStart, handleTryAgain } = useGlobalContext();
-  return <></>;
+  return (
+    <>
+      <Container className="p-3 d-flex align-items-center min-vh-100">
+        <Jumbotron className="score text-center mx-auto">
+          <h1 className="header mb-5 display-1">
+            Your Score <br></br>
+            {score}/{quizz.length}
+          </h1>
+          {/* {alert.show && <Alert {...alert} />} */}
+
+          <Button variant="secondary" size="lg" onClick={handleStart}>
+            Try another Level
+          </Button>
+          <Button className="btn-brown" size="lg m-2" onClick={handleTryAgain}>
+            Try again
+          </Button>
+        </Jumbotron>
+      </Container>
+    </>
+  );
 };
 
 export default Gameover;
