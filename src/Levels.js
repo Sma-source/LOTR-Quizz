9d0825df863f0348013d24d7d91eced366d25945
Sma-source <rabhi.smain@gmail.com>
2021-04-09 20:59:03 +0200

    fix warning

--- src/Levels.js
+++ src/Levels.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useGlobalContext } from "./context/GlobalState";
-import Button from "react-bootstrap/Button";
 import FrodonSvg from "./FrodonSvg";
 import LegolasSvg from "./LegolasSvg";
 import GandalfSvg from "./GandalfSvg";
